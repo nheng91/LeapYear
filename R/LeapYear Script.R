38a464ca3afdca99c6415d531cbe8820afc8df98
Nate Heng <nheng91@gmail.com>
2018-03-05 08:25:01 -0600

    Committing fixes to leap_year function

--- R/LeapYear Script.R
+++ R/LeapYear Script.R
@@ -7,8 +7,10 @@
 #'
 #'@export
 leap_year <- function(year) {
-  stopifnot(nchar(year) == 4, length(year) == 1, is.numeric(year))
-  if(year %in% leap_years){
+  if(nchar(year) != 4 | length(year) != 1 | is.numeric(year) == FALSE){
+    output <- "Error in year entered.  Please enter one 4 digit number between 1804 and 2400"
+  }
+  else if(year %in% leap_years){
     output <- paste(year,"is a leap year.")
   } else{
     output <- paste(year,"is not a leap year.")
